7005d17c46aa535fcb6bf69a24d2e9d9dcde7183
MD SAFAYAT HOSSAIN <110348737+SH-KHAN-SIFAT@users.noreply.github.com>
2023-02-07 20:40:52 +0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,96 @@
+"""print("Hello Sarima")
+
+age = 2
+
+print(age)
+
+year = 1
+month = 9
+day = 6
+
+print(str(year) + "Year" + " " + str(month) + "Months" + " " + str(day) + "Days")
+
+name = input("What is your name?\n" )
+
+print(name)
+
+first = input("First Number = " )
+first = float(input("First Number = " ))
+second = input("Second Number = " )
+second = float(input("Second Number = " ))
+
+sum = float(first) + float(second)
+
+print(sum)
+
+test = "Umme Sarima Ferdous"
+
+print(test.upper())
+print(test.lower())
+print(test.find("S"))
+print(test.replace("Umme Sarima Ferdous" , "MD.Safayat Hossain"))
+print("SH" in test)
+
+print(9+1)
+print(11-1)
+print(2*5)
+print(100/11)
+print(2**3)
+print(10//3)
+print(10%3)
+print(3+10*2)
+print((3+10)*2)
+
+x = 3>2;    y = 3<2;     z = 3==2;     A = 3>=2;     B = 3<=2;      C = 3!=2
+
+print(x)
+print(y)
+print(z)
+print(A)
+print(B)
+print(C)
+
+height = 6
+
+print(height>5 and height<10)
+print(height<5 and height<10)
+
+print(height<5 or height<10)
+print(height<5 or height>10)
+
+print(height<10)
+print(not height<10)
+
+temperature = 25
+
+if temperature>30:
+    print("It's a hot day")
+elif temperature>20:
+    print("It's a nice day")
+elif temperature>10:
+    print("It's bit cold day")
+else:
+    print("It's cold day")
+
+print("Done")
+
+weight = float(input("Weight = " ))
+abc = input("(K)g or (L)bs :" )
+
+if abc == "K" or abc == "k" :
+    print("Weight in Lbs : ")
+    print(weight*2.22)
+elif abc == "L" or abc == "l" :
+    print("Weight in Kg : ")
+    print(weight/2.22)
+else :
+    print("Invalid Input")
+
+i = 1
+while i<10:
+     print(i * "*")
+     i = i + 1
+"""
+
+
+
